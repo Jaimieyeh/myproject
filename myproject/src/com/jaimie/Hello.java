af8c60b1c97b088480d4bb9be96fdb820db9b894
user <user@es302n27>
2018-09-25 12:02:33 +0800

    initial

---
+++ myproject/src/com/jaimie/Hello.java
@@ -0,0 +1,21 @@
+package com.jaimie;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		short age = 19;
+		System.out.println(age);
+		int number = 216636546;
+		System.out.println(number);
+		float weight = (float)55.5;		
+		System.out.println(weight); 
+		double height = 123;
+		System.out.println(height);
+		System.out.print("hello, ");
+		System.out.println("world!");
+		
+	
+	}
+
+}
